f1d646beb40bf738804931bd9a591088297d40e5
Ruben Patel <rubenpatel71@gmail.com>
2021-06-08 18:35:03 +0200

    Improved cleanup

--- reportgeneration/DockerMain.py
+++ reportgeneration/DockerMain.py
@@ -1,5 +1,6 @@
 
 import os
+import traceback
 import xarray as xr
 import sys
 import shutil
@@ -32,19 +33,19 @@ class DockerMain :
         self.datain, self.predin, self.dataout = mdirs
 
         self.output_type = os.getenv('OUTPUT_TYPE', 'zarr')
-        self.main_freq = os.getenv('MAIN_FREQ', 38000)
-        self.max_range = os.getenv('MAX_RANGE_SRC', 500)
+        self.main_freq = float(os.getenv('MAIN_FREQ', 38000))
+        self.max_range = float(os.getenv('MAX_RANGE_SRC', 500))
 
         self.data_input_name = os.getenv('DATA_INPUT_NAME', None)
         self.pred_input_name = os.getenv('PRED_INPUT_NAME', None)
         self.bot_input_name = os.getenv('BOT_INPUT_NAME', None)
         self.output_name = os.getenv('OUTPUT_NAME', None)
         self.write_png = os.getenv('WRITE_PNG', None)
-        self.threshold = os.getenv('THRESHOLD', None)
+        self.threshold = float(os.getenv('THRESHOLD', None))
         self.hitype = os.getenv('HOR_INTEGRATION_TYPE', None)
-        self.histep = os.getenv('HOR_INTEGRATION_STEP', None)
+        self.histep = float(os.getenv('HOR_INTEGRATION_STEP', None))
         self.vitype = os.getenv('VERT_INTEGRATION_TYPE', None)
-        self.vistep = os.getenv('VERT_INTEGRATION_STEP', None)
+        self.vistep = float(os.getenv('VERT_INTEGRATION_STEP', None))
 
         if self.data_input_name is None:
             print('DATA_INPUT_NAME no set')
@@ -55,10 +56,10 @@ class DockerMain :
             return False
 
         if self.bot_input_name is not None:
-            print('BOT_INPUT_NAME. Masking with bottom data not implimented. Continu without bottom data')
+            print('BOT_INPUT_NAME. Masking with bottom data not implemented. Continu without bottom data')
 
         if self.output_name is None:
-            print('INPUT_NAME no set')
+            print('OUTPUT_NAME no set')
             return False
 
         if self.threshold is None:
@@ -66,11 +67,11 @@ class DockerMain :
             return False
 
         if not isinstance(self.threshold, float):
-            print('THRESHOLD needs to be float [0,1]')
+            print('THRESHOLD needs to be float, got {}'.format(type(self.threshold)))
             return False
 
         if self.threshold<0 or self.threshold>1:
-            print('THRESHOLD needs to be float [0,1]')
+            print('THRESHOLD needs to be float [0,1], got {}'.format(self.threshold))
             return False
 
         if self.hitype is None or self.hitype not in ['ping', 'time']:
@@ -91,6 +92,27 @@ class DockerMain :
 
         return True
 
+    def usage(self):
+        return \
+            'docker run -it --name reportgenerator\n \
+            -v /data/cruise_data/2020/S2020842_PHELMERHANSSEN_1173/ACOUSTIC/PREPROCESSED:/datain\n \
+            -v /data/cruise_data/2020/S2020842_PHELMERHANSSEN_1173/ACOUSTIC/PRDICTIONS:/predin\n \
+            -v /data/cruise_data/2020/S2020842_PHELMERHANSSEN_1173/ACOUSTIC/BOTTOM:/botin (optional)\n \
+            -v /data/cruise_data/2020/S2020842_PHELMERHANSSEN_1173/ACOUSTIC/OUT:/dataout\n \
+            --security-opt label=disable\n \
+            --env DATA_INPUT_NAME=input_filename.zarr\n \
+            --env PRED_INPUT_NAME=prediction_filename.zarr\n \
+            --env BOT_INPUT_NAME=bottom_filename.zarr (optional)\n\
+            --env OUTPUT_NAME=result.zarr\n\
+            --env WRITE_PNG=result.png\n \
+            --env THRESHOLD=0.8\n \
+            --env MAIN_FREQ = 38000\n \
+            --env MAX_RANGE_SRC = 500\n \
+            --env HOR_INTEGRATION_TYPE = ping [ping | time | nmi]\n  \
+            --env HOR_INTEGRATION_STEP = 100\n \
+            --env VERT_INTEGRATION_TYPE=range\n \
+            --env VERT_INTEGRATION_STEP=10\n \
+            reportgenerator'
 
     def run(self):
 
@@ -123,7 +145,6 @@ if __name__ == '__main__':
         input()
         sys.exit(0)
 
-
     dm = DockerMain()
 
     if dm.goodtogo:
@@ -135,12 +156,17 @@ if __name__ == '__main__':
         client = Client()
         print(client)
 
-        dm.run()
+        try:
+            dm.run()
+        except :
+            traceback.print_exc()
 
-        # Cleaning up
         client.close()
         if os.path.exists(tmp_dir):
             shutil.rmtree(tmp_dir)
 
     else :
-        print('Error occured, exiting')
+        print('Error occurred, exiting')
+        print('Usage :/n{}'.format(dm.usage()))
+
+    sys.exit(0)
