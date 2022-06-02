3971111a15a16d317d7827dc83ff2d5ad4b72b39
nilsolav <nils.olav.handegard@imr.no>
2022-06-02 10:51:19 +0200

    Writes csv with standard variable names

--- test_S2019847_0511.py
+++ test_S2019847_0511.py
@@ -25,18 +25,22 @@ PingAxisIntervalType = "distance"  # see http://vocab.ices.dk/?ref=1455
 PingAxisIntervalOrigin = "start"  # see http://vocab.ices.dk/?ref=1457
 PingAxisIntervalUnit = "nmi"  # see http://vocab.ices.dk/?ref=1456
 PingAxisInterval = 0.1
+# Ruben: refactor these to ICESAcoustic variable names:
+hitype = PingAxisIntervalUnit
+histep = PingAxisInterval
+
+ChannelDepthIntervalStart = 0  # Integration start depth (not implemented)
+vistep = 10
+max_range = 500
+
+# CRIMAC regridder parameters
 SvThreshold = 0  # db eller lineære verdiar?
 Type = "C"  # C = sA, Nautical area scattering coefficient
 Unit = "m2nmi-2"  # see http://vocab.ices.dk/?ref=1460 |
-
-# CRIMAC regridder parameters
 main_freq = 38000
-threshold = 0.8
-vitype = 'range'#'depth'
-vistep = 10
-hitype = PingAxisIntervalUnit
-histep = PingAxisInterval
-max_range = 500
+threshold = 0.8  # dont think this is needed. The allocation is not binary.
+
+vitype = 'range'  # 'depth'
 
 rep = rg.Reportgenerator(grid_file_name,
                          pred_file_name,
@@ -53,14 +57,77 @@ rep = rg.Reportgenerator(grid_file_name,
 rep.save(report_file_name)
 rep.save(report_file_name+'.png')
 
+
 #
-# Testing
+# Saving to ICESAcoustic format
 #
+
+# (Ruben: This part needs to be added to the main classes)
 
 # Reading the report & original data
 grid = xr.open_zarr(grid_file_name)
 pred = xr.open_zarr(pred_file_name)
-rep = xr.open_zarr(report_file_name)
+report = xr.open_zarr(report_file_name)
+
+# Rename coordinates and variables
+report = report.rename({'latitude': 'Latitude',
+                        'longitude': 'Longitude',
+                        'ping_time': 'Time',
+                        'sv': 'value',
+                        'range': 'ChannelDepthUpper',
+                        'category': 'SaCategory'})
+
+# Add new coordinates
+N = len(report.Time)
+Latitude2 = np.append(report.Latitude[1:].values, np.NaN)  # Adress end point
+Longitude2 = np.append(report.Longitude[1:].values, np.NaN)  # Address last
+Origin = np.repeat("start", N)
+Origin2 = np.repeat("end", N)
+BottomDepth = np.repeat(np.nan, N)  # Needs to be added from input data
+Validity = np.repeat("V", N)
+# Upper depth of the integrator cell in meters relative surface
+ChannelDepthLower = np.append(
+    report.ChannelDepthUpper[1:].values, np.NaN)  # Adress end point
+
+report = report.assign_coords(Latitude2=("Time", Latitude2))
+report = report.assign_coords(Longitude2=("Time", Longitude2))
+report = report.assign_coords(Origin=("Time", Origin))
+report = report.assign_coords(Origin2=("Time", Origin2))
+report = report.assign_coords(BottomDepth=("Time", BottomDepth))
+report = report.assign_coords(Validity=("Time", Validity))
+report = report.assign_coords(ChannelDepthLower=("ChannelDepthUpper",
+                                                 ChannelDepthLower))
+
+# Ruben: Is this log distance?:
+report = report.assign_coords(Distance=("Time", report.distance[1, :].values))
+
+# Add attributes
+report = report.assign_attrs({"PingAxisIntervalType": PingAxisIntervalType,
+                              "PingAxisIntervalOrigin": PingAxisIntervalOrigin,
+                              "PingAxisIntervalUnit": PingAxisIntervalUnit,
+                              "PingAxisInterval": PingAxisInterval,
+                              "Platform": "NaN",
+                              "LocalID": "NaN",
+                              "Type": Type,
+                              "Unit": Unit})
+
+# (Ruben: end refactoring)
+
+#
+# Flatten the data to a dataframe
+#
+
+df = report.to_dataframe()
+# Add the attributes to the df
+for item in list(report.attrs.items()):
+    df[item[0]] = item[1]
+# Save report to pandas tidy file
+df.to_csv(report_file_name+'.csv', index=True)
+
+#
+# Testing
+#
+
 
 # Comparing the integrator with xarray averaging
 
@@ -73,59 +140,11 @@ Sa_raw = 10*np.log10(hres+0.00000001)
 
 # Do the same for the output from the integrator. Should be similar'sih
 # as the plot from the original data
-lres = rep.sv.sel(category=27).mean(dim='range').resample(
-            ping_time='H').mean() * max_range
+lres = report.value.sel(SaCategory=27).mean(dim='ChannelDepthUpper').resample(
+            Time='H').mean() * max_range
 Sa_int = 10*np.log10(lres+0.00000001)
 
 fig, axes = plt.subplots(nrows=2)
 Sa_int.plot(ax=axes[0])
 Sa_raw.plot(ax=axes[1])
 plt.savefig(report_file_name+'_sv.png')
-
-#
-# Saving to ICESAcoustic format
-#
-
-# Rename coordinates and variables
-rep = rep.rename({'latitude': 'Latitude',
-                  'longitude': 'Longitude',
-                  'ping_time': 'Time',
-                  'sv': 'value',
-                  'category': 'SaCategory'})
-
-# Add new coordinates
-N = len(rep.Time)
-Latitude2 = np.append(rep.LogLatitude[1:].values, np.NaN)
-Longitude2 = np.append(rep.LogLongitude[1:].values, np.NaN)
-Origin = np.repeat("start", N)
-Origin2 = np.repeat("end", N)
-BottomDepth = np.repeat(np.nan, N)  # Needs to be added from input data
-Validity = np.repeat("V", N)
-
-rep.assign_coords(Latitude2=("Time", Latitude2))
-rep.assign_coords(Longitude2=("Time", Longitude2))
-rep.assign_coords(Origin=("Time", Origin))
-rep.assign_coords(Origin2=("Time", Origin2))
-rep.assign_coords(BottomDepth=("Time", BottomDepth))
-rep.assign_coords(Validity=("Time", Validity))
-rep.assign_coords(Distance=("Time", rep.distance.data))
-
-rep.assign_attrs({"PingAxisIntervalType": PingAxisIntervalType,
-                  "PingAxisIntervalOrigin": PingAxisIntervalOrigin,
-                  "PingAxisIntervalUnit": PingAxisIntervalUnit,
-                  "PingAxisInterval": PingAxisInterval,
-                  "Platform": "NaN",
-                  "LocalID": "NaN"})
-rep.value.assign_attrs({"Type": Type,
-                        "Unit": Unit})
-
-# Flatten the data to a dataframe
-df = rep.to_dataframe()
-"""
-  | Sample | ChannelDepthUpper |  | Upper depth of the integrator cell in meters relative surface |
-  | Sample | ChannelDepthLower |  | Lower depth of the integrator cell in meters relative surface |
-"""
-
-
-# Save report to pandas tidy file
-rep.to_dataframe().to_csv(report_file_name+'.csv')
