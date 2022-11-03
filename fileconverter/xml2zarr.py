d923fda9cad5ac6644ca7183474d9c4087e65d62
Nils Olav Handegard <nils.olav.handegard@imr.no>
2022-11-03 14:18:57 +0100

    File and dir for addint xml converter

---
+++ fileconverter/xml2zarr.py
@@ -0,0 +1,9 @@
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+
+# test data
+rawdir = '/mnt/c/DATAscratch/crimac-scratch/2018/S2018823/ACOUSTIC/REPORTS/'
+xmlfile = 'echosounder_cruiseNumber_2018823_Eros_2021-02-05T00.06.32.305Z.xml'  # Official data
+zarrfile = 'S2018823_report_0.zarr'  # Naming convetion for files converted from standard estimates
+
